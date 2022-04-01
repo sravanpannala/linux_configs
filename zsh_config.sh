80bc831bf3247de16d16df4cdec5a47754e0cf20
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-03-31 21:59:38 -0400

    zsh_aliases

---
+++ zsh_config.sh
@@ -0,0 +1,6 @@
+yay -S zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-completions nerd-fonts-fira-code oh-my-posh-git fzf find-the-command-git --needed
+
+cp ./.zshrc  ~/.zshrc 
+cp ./.zsh_aliases  ~/.zsh_aliases 
+touch ~/.histfile
+cp ./stoys.omp.json ~/stoys.omp.json
