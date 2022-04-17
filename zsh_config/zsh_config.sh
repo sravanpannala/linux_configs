ad4b78246ce39a550a6d4dedd726cb376fec2080
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-04-17 16:46:10 -0400

    fix comments

--- zsh_config/zsh_config.sh
+++ zsh_config/zsh_config.sh
@@ -1,8 +1,8 @@
-# yay -S zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-completions nerd-fonts-fira-code oh-my-posh-git fzf find-the-command-git --needed
+yay -S zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-completions nerd-fonts-fira-code oh-my-posh-git fzf find-the-command-git --needed
 
 ln -s -f $(pwd)/.zshrc  ~/ 
 ln -s -f $(pwd)/.zsh_aliases  ~/ 
-# touch ~/.histfile
+touch ~/.histfile
 ln -s -f $(pwd)/stoys.omp.json ~/
 
-# chsh -s /bin/zsh
+chsh -s /bin/zsh
